4eeda23055535ef12f30880703f2c6d6b26a2112
Tolunay Tek <tek_tolunay@live.de>
2018-11-19 10:33:23 +0100

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,5 @@
 
 @section('content')
 <div class="container WelcomeDiv">
-        <a class="btn btn-primary" href="/login">Login</a>
-        <a class="btn btn-secondary" href="/register">Register</a>
-        </p>
     </div>
 @endsection
