d46e55b0a5dc6f6ff0cfa4f8f92d8cd2b4b211b3
Nair <nair.puagic@gmx.ch>
2018-11-19 11:00:19 +0100

    nev template file

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,25 @@
+    <!-- start navigation -->
+    <div class="navbar navbar-fixed-top navbar-default" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                    <span class="icon icon-bar"></span>
+                </button>
+                <a href="#" class="navbar-brand"><img src="images/logo.png" class="img-responsive" alt="logo"></a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#home" class="smoothScroll">HOME</a></li>
+                    <li><a href="#service" class="smoothScroll">SERVICE</a></li>
+                    <li><a href="#about" class="smoothScroll">ABOUT</a></li>
+                    <li><a href="#team" class="smoothScroll">TEAM</a></li>
+                    <li><a href="#pricing" class="smoothScroll">PRICING</a></li>
+                    <li><a href="#portfolio" class="smoothScroll">PORTFOLIO</a></li>
+                    <li><a href="#contact" class="smoothScroll">CONTACT</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- end navigation -->
