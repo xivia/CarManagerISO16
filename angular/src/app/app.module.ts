4e636bd4da7abb25b477c14b2d0611fd0613bc88
Sandro Bolliger <sandro.bolliger@vglsy.com>
2018-12-03 11:06:54 +0100

    updated app.module

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
 import { NavComponent } from './nav/nav.component';
-=======
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -19,17 +17,13 @@ const routes: Routes = [
   { path: 'settings', component: SettingsComponent },
   { path: '**', component: AppComponent },
 ];
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< Updated upstream
-    NavComponent
-=======
+    NavComponent,
     CreateCarComponent,
     SettingsComponent
->>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
