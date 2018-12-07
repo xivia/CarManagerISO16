a0d22a1c70e0518fb0024074fda4a693157640c3
Sandro Bolliger <sandro.bolliger@vglsy.com>
2018-12-07 14:04:09 +0100

    updated the navbar (the old navbar is still in the file as a comment)

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,4 +1,45 @@
-    <!-- start navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/home') }}">
+            {{ config('app.name', 'CoolCars') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/create">Create a new Car</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/settings">Settings</a>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                @else
+                <a class="nav-link" href="/showProfile">Profile</a>
+                <a class="nav-link" href="/editProfile">Edit Profile</a>
+                <div class="dropdown-divider"></div>
+                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{__('Logout') }}</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+<!--
+start navigation
     <div class="navbar navbar-fixed-top navbar-default" role="navigation">
         <div class="container">
             <div class="navbar-header">
@@ -22,4 +63,5 @@
             </div>
         </div>
     </div>
-    <!-- end navigation -->
+ end navigation
+-->
