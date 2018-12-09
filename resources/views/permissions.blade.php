c2ed247dfc41ea96d88e67221d8ee79a25aaf316
unknown <nair.pujagic@gmx.ch>
2018-12-09 22:15:25 +0100

    tolu fucked up

---
+++ resources/views/permissions.blade.php
@@ -0,0 +1,58 @@
+@extends ('layouts.app')
+@section ('content')
+<div class="container">
+<ul>
+ 		@foreach ($errors->all() as $error) 
+	
+ 			<li>{{$error}}</li>
+ 		@endforeach
+</ul>
+
+<table class="table">
+	<tr>
+	<th scope="col">Name</th>
+	<th scope="col">Permissions</th>
+	<th scope="col">Update</th>
+	<th scope="col">Execute</th>
+	<th scope="col">Delete User</th>
+	</tr>
+
+@foreach($users as $user)
+	
+<tr>
+	<td>{{$user->name}}</td>
+	<td>{{$user->permission->name}}</td>
+  	<form method="post" action="/admin/update">
+  		<input name="user" type="hidden" value="{{$user->id}}">
+  		{{ method_field('patch') }}
+  		{{ csrf_field() }}
+   		<td>
+    	  
+    		<select name=permission class="form-control">
+		  		@foreach ($permissions = App\permission::all() as $permission)
+			
+			    <option value="{{$permission->id}}">{{$permission->name}}</option>   
+			
+				@endforeach
+			</select>
+    	  
+        	 
+    	  </div>
+
+	    </td>
+	    <td>
+	    	<button type="submit" class="btn btn-danger col-lg-8">Update Permissions</button>
+	    </td>
+    </form><td>
+      	<form method="post" action="/admin/update">
+  		<input name="user" type="hidden" value="{{$user->id}}">
+  		{{ method_field('delete') }}
+  		{{ csrf_field() }}
+  		<button type="submit" class="btn btn-danger col-lg-12">Delete User</button>
+  		</form>
+  	</td>
+</tr>
+@endforeach
+</table>
+</div>
+@endsection
